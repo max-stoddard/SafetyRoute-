48d066a0fc76f54a0f229632bc7cc86f5cbd4c5d
Ryan OD <58502747+Ryan7od@users.noreply.github.com>
2025-02-01 17:44:17 +0000

    feat(rF): Setup skeleton methods

--- src/python/routeFinder.py
+++ src/python/routeFinder.py
@@ -6,7 +6,18 @@ CORS(app)
 
 
 @app.route('/api/calculate_route', methods=['GET'])
-def test():
+def calculate_route():
+
+    find_routes()
+
+    for route in routes:
+        create_coordinate_pairs()
+        create_list_of_crimes()
+        create_list_of_scores()
+        create_one_score()
+
+    find_best_route()
+
     return jsonify({'message': 'GET request successful'})
 
 
