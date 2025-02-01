3b39d35db0c77a246207911a952eac0504bd4b4a
VeerV11 <veervishalvij@gmail.com>
2025-02-01 21:37:57 +0000

    feat: create model to regress vectors into score

---
+++ src/python/vectorAnalyze.py
@@ -0,0 +1,15 @@
+from sklearn.linear_model import LinearRegression
+
+class ContextRegressor:
+    
+    def __init__(self):
+        self.model = LinearRegression()
+    
+
+    def train(self, X, y):
+        self.model.fit(X, y)
+    
+    def predict(self, X):
+        return self.model.predict(X)
+    
+
