cd8ae8fd31594c2c32af08307f40c7ebb92a3424
wc1023 <william.chen23@imperial.ac.uk>
2025-02-02 05:50:02 +0000

    feat: Add search with start and end markers

--- src/ReactJS/app/index.jsx
+++ src/ReactJS/app/index.jsx
@@ -21,69 +21,69 @@ import { GOOGLE_API_KEY } from '@env';
 const windowWidth = Dimensions.get("window").width;
 const windowHeight = Dimensions.get("window").height;
 
+
 // Define the route coordinates
-// const routeCoordinates = [];
-const routeCoordinates = [
-  { latitude: 51.48209, longitude: -0.19003 },
-  { latitude: 51.48167, longitude: -0.18956 },
-  { latitude: 51.48157, longitude: -0.18947 },
-  { latitude: 51.48153, longitude: -0.18946 },
-  { latitude: 51.48146, longitude: -0.18951 },
-  { latitude: 51.48138, longitude: -0.18953 },
-  { latitude: 51.4812, longitude: -0.18946 },
-  { latitude: 51.48111, longitude: -0.18944 },
-  { latitude: 51.48106, longitude: -0.18948 },
-  { latitude: 51.481, longitude: -0.18937 },
-  { latitude: 51.48087, longitude: -0.18924 },
-  { latitude: 51.48082, longitude: -0.18913 },
-  { latitude: 51.48058, longitude: -0.18887 },
-  { latitude: 51.4806, longitude: -0.18883 },
-  { latitude: 51.4807, longitude: -0.1887 },
-  { latitude: 51.48096, longitude: -0.18838 },
-  { latitude: 51.48112, longitude: -0.18816 },
-  { latitude: 51.48134, longitude: -0.18783 },
-  { latitude: 51.48147, longitude: -0.18768 },
-  { latitude: 51.48162, longitude: -0.18751 },
-  { latitude: 51.48169, longitude: -0.18745 },
-  { latitude: 51.48177, longitude: -0.18741 },
-  { latitude: 51.48193, longitude: -0.18726 },
-  { latitude: 51.4824, longitude: -0.18681 },
-  { latitude: 51.48262, longitude: -0.18657 },
-  { latitude: 51.48271, longitude: -0.18646 },
-  { latitude: 51.48288, longitude: -0.18628 },
-  { latitude: 51.48307, longitude: -0.18601 },
-  { latitude: 51.48311, longitude: -0.18595 },
-  { latitude: 51.48314, longitude: -0.18586 },
-  { latitude: 51.48318, longitude: -0.18592 },
-  { latitude: 51.48332, longitude: -0.1861 },
-  { latitude: 51.48382, longitude: -0.18678 },
-  { latitude: 51.48408, longitude: -0.18717 },
-  { latitude: 51.48446, longitude: -0.18778 },
-  { latitude: 51.48462, longitude: -0.18805 },
-  { latitude: 51.48512, longitude: -0.18882 },
-  { latitude: 51.48603, longitude: -0.18978 },
-  { latitude: 51.48666, longitude: -0.19044 },
-  { latitude: 51.48683, longitude: -0.19061 },
-  { latitude: 51.48691, longitude: -0.19066 },
-  { latitude: 51.48697, longitude: -0.19069 },
-  { latitude: 51.48708, longitude: -0.19086 },
-  { latitude: 51.48761, longitude: -0.19143 },
-  { latitude: 51.48816, longitude: -0.19199 },
-  { latitude: 51.48858, longitude: -0.19246 },
-  { latitude: 51.48861, longitude: -0.19252 },
-  { latitude: 51.48865, longitude: -0.19269 },
-  { latitude: 51.48867, longitude: -0.19276 },
-  { latitude: 51.48869, longitude: -0.1927 },
-  { latitude: 51.48868, longitude: -0.19264 },
-  { latitude: 51.48873, longitude: -0.1925 },
-  { latitude: 51.48875, longitude: -0.19259 },
-  { latitude: 51.48882, longitude: -0.19275 },
-  { latitude: 51.48886, longitude: -0.19297 },
-  { latitude: 51.4889, longitude: -0.19311 },
-  { latitude: 51.48913, longitude: -0.1935 },
-  { latitude: 51.48953, longitude: -0.19425 },
-  { latitude: 51.48993, longitude: -0.19502 },
-];
+const routeCoordinates = [];
+//   { latitude: 51.48209, longitude: -0.19003 },
+//   { latitude: 51.48167, longitude: -0.18956 },
+//   { latitude: 51.48157, longitude: -0.18947 },
+//   { latitude: 51.48153, longitude: -0.18946 },
+//   { latitude: 51.48146, longitude: -0.18951 },
+//   { latitude: 51.48138, longitude: -0.18953 },
+//   { latitude: 51.4812, longitude: -0.18946 },
+//   { latitude: 51.48111, longitude: -0.18944 },
+//   { latitude: 51.48106, longitude: -0.18948 },
+//   { latitude: 51.481, longitude: -0.18937 },
+//   { latitude: 51.48087, longitude: -0.18924 },
+//   { latitude: 51.48082, longitude: -0.18913 },
+//   { latitude: 51.48058, longitude: -0.18887 },
+//   { latitude: 51.4806, longitude: -0.18883 },
+//   { latitude: 51.4807, longitude: -0.1887 },
+//   { latitude: 51.48096, longitude: -0.18838 },
+//   { latitude: 51.48112, longitude: -0.18816 },
+//   { latitude: 51.48134, longitude: -0.18783 },
+//   { latitude: 51.48147, longitude: -0.18768 },
+//   { latitude: 51.48162, longitude: -0.18751 },
+//   { latitude: 51.48169, longitude: -0.18745 },
+//   { latitude: 51.48177, longitude: -0.18741 },
+//   { latitude: 51.48193, longitude: -0.18726 },
+//   { latitude: 51.4824, longitude: -0.18681 },
+//   { latitude: 51.48262, longitude: -0.18657 },
+//   { latitude: 51.48271, longitude: -0.18646 },
+//   { latitude: 51.48288, longitude: -0.18628 },
+//   { latitude: 51.48307, longitude: -0.18601 },
+//   { latitude: 51.48311, longitude: -0.18595 },
+//   { latitude: 51.48314, longitude: -0.18586 },
+//   { latitude: 51.48318, longitude: -0.18592 },
+//   { latitude: 51.48332, longitude: -0.1861 },
+//   { latitude: 51.48382, longitude: -0.18678 },
+//   { latitude: 51.48408, longitude: -0.18717 },
+//   { latitude: 51.48446, longitude: -0.18778 },
+//   { latitude: 51.48462, longitude: -0.18805 },
+//   { latitude: 51.48512, longitude: -0.18882 },
+//   { latitude: 51.48603, longitude: -0.18978 },
+//   { latitude: 51.48666, longitude: -0.19044 },
+//   { latitude: 51.48683, longitude: -0.19061 },
+//   { latitude: 51.48691, longitude: -0.19066 },
+//   { latitude: 51.48697, longitude: -0.19069 },
+//   { latitude: 51.48708, longitude: -0.19086 },
+//   { latitude: 51.48761, longitude: -0.19143 },
+//   { latitude: 51.48816, longitude: -0.19199 },
+//   { latitude: 51.48858, longitude: -0.19246 },
+//   { latitude: 51.48861, longitude: -0.19252 },
+//   { latitude: 51.48865, longitude: -0.19269 },
+//   { latitude: 51.48867, longitude: -0.19276 },
+//   { latitude: 51.48869, longitude: -0.1927 },
+//   { latitude: 51.48868, longitude: -0.19264 },
+//   { latitude: 51.48873, longitude: -0.1925 },
+//   { latitude: 51.48875, longitude: -0.19259 },
+//   { latitude: 51.48882, longitude: -0.19275 },
+//   { latitude: 51.48886, longitude: -0.19297 },
+//   { latitude: 51.4889, longitude: -0.19311 },
+//   { latitude: 51.48913, longitude: -0.1935 },
+//   { latitude: 51.48953, longitude: -0.19425 },
+//   { latitude: 51.48993, longitude: -0.19502 },
+// ];
 
 
 export default function App() {
@@ -93,9 +93,11 @@ export default function App() {
   const [text2, setText2] = useState('');
   const [suggestions1, setSuggestions1] = useState([]);
   const [suggestions2, setSuggestions2] = useState([]);
+  const [start, setStart] = useState(null); // Coordinates for current location
+  const [end, setEnd] = useState(null);     // Coordinates for destination
   const mapRef = React.useRef(null);
 
-  // Get the current location and set the initial region
+  // Get current location and set the initial region
   useEffect(() => {
     const getLocation = async () => {
       let { status } = await Location.requestForegroundPermissionsAsync();
@@ -103,10 +105,8 @@ export default function App() {
         console.log("Permission to access location was denied");
         return;
       }
-
       let location = await Location.getCurrentPositionAsync({});
       setCurrentLocation(location.coords);
-
       if (routeCoordinates.length > 0) {
         setInitialRegion({
           latitude: routeCoordinates[0].latitude,
@@ -123,11 +123,10 @@ export default function App() {
         });
       }
     };
-
     getLocation();
   }, []);
 
-  // Fetch suggestions for the first text input (Current Location)
+  // Fetch autocomplete suggestions for text1 (Current Location)
   useEffect(() => {
     if (text1.length >= 3) {
       fetch(
@@ -152,7 +151,7 @@ export default function App() {
     }
   }, [text1]);
 
-  // Fetch suggestions for the second text input (Destination)
+  // Fetch autocomplete suggestions for text2 (Destination)
   useEffect(() => {
     if (text2.length >= 3) {
       fetch(
@@ -177,15 +176,62 @@ export default function App() {
     }
   }, [text2]);
 
-  // When a suggestion is pressed, update the text and clear suggestions.
-  const handleSuggestionPress1 = (description) => {
-    setText1(description);
-    setSuggestions1([]);
+   // Helper to recenter the map on a given coordinate
+   const recenterMap = (coordinate) => {
+    if (mapRef.current && coordinate) {
+      mapRef.current.animateToRegion({
+        latitude: coordinate.latitude,
+        longitude: coordinate.longitude,
+        latitudeDelta: 0.01,
+        longitudeDelta: 0.01,
+      });
+    }
   };
 
-  const handleSuggestionPress2 = (description) => {
-    setText2(description);
+  // When a suggestion is selected for the current location, fetch its details
+  const handleSuggestionPress1 = (item) => {
+    setText1(item.description);
+    setSuggestions1([]);
+    fetch(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${
+       item.place_id
+      }&key=${GOOGLE_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "OK") {
+          const location = data.result.geometry.location;
+          const coordinate = { latitude: location.lat, longitude: location.lng };
+          setStart(coordinate);
+          recenterMap(coordinate);
+        } else {
+          console.error("Place details error:", data.error_message);
+        }
+      })
+      .catch((error) => console.error("Fetch error:", error));
+  };
+
+  // When a suggestion is selected for the destination, fetch its details
+  const handleSuggestionPress2 = (item) => {
+    setText2(item.description);
     setSuggestions2([]);
+    fetch(
+      `https://maps.googleapis.com/maps/api/place/details/json?place_id=${
+       item.place_id
+      }&key=${GOOGLE_API_KEY}`
+    )
+      .then((response) => response.json())
+      .then((data) => {
+        if (data.status === "OK") {
+          const location = data.result.geometry.location;
+          const coordinate = { latitude: location.lat, longitude: location.lng };
+          setEnd(coordinate);
+          recenterMap(coordinate);
+        } else {
+          console.error("Place details error:", data.error_message);
+        }
+      })
+      .catch((error) => console.error("Fetch error:", error));
   };
 
   const recenterToRoute = () => {
@@ -218,11 +264,11 @@ export default function App() {
     }
   };
 
-  // Render a suggestion item for the dropdown list
+  // Render a suggestion item in the dropdown
   const renderSuggestionItem = ({ item }, onPressHandler) => (
     <TouchableOpacity
       style={styles.suggestionItem}
-      onPress={() => onPressHandler(item.description)}
+      onPress={() => onPressHandler(item)}
     >
       <Text style={styles.suggestionText}>{item.description}</Text>
     </TouchableOpacity>
@@ -232,26 +278,13 @@ export default function App() {
     <TouchableWithoutFeedback onPress={Keyboard.dismiss} accessible={false}>
       <View style={styles.container}>
         {initialRegion && (
-          <MapView
-            ref={mapRef}
-            style={styles.map}
-            initialRegion={initialRegion}
-          >
+          <MapView ref={mapRef} style={styles.map} initialRegion={initialRegion}>
             {/* Draw Route on Map */}
             {routeCoordinates.length > 0 && (
               <>
-                <Polyline
-                  coordinates={routeCoordinates}
-                  strokeWidth={5}
-                  strokeColor="blue"
-                />
+                <Polyline coordinates={routeCoordinates} strokeWidth={5} strokeColor="blue" />
                 <Marker coordinate={routeCoordinates[0]} title="Start" />
-                <Marker
-                  coordinate={
-                    routeCoordinates[routeCoordinates.length - 1]
-                  }
-                  title="End"
-                />
+                <Marker coordinate={routeCoordinates[routeCoordinates.length - 1]} title="End" />
               </>
             )}
 
@@ -264,46 +297,39 @@ export default function App() {
                 </View>
               </Marker>
             )}
+
+            {/* Optionally, render markers for the fetched start and end positions */}
+            {start && (
+              <Marker coordinate={start} pinColor="green" title="Selected Start" />
+            )}
+            {end && (
+              <Marker coordinate={end} pinColor="red" title="Selected End" />
+            )}
           </MapView>
         )}
 
         {/* Profile Icon */}
-        <TouchableOpacity
-          style={styles.profileButton}
-          onPress={() => console.log("Profile Pressed")}
-        >
+        <TouchableOpacity style={styles.profileButton} onPress={() => console.log("Profile Pressed")}>
           <Ionicons name="person-circle" size={50} color="white" />
         </TouchableOpacity>
 
         {/* Left Button - Recenter Route */}
-        <TouchableOpacity
-          style={styles.recenterRouteButton}
-          onPress={recenterToRoute}
-        >
+        <TouchableOpacity style={styles.recenterRouteButton} onPress={recenterToRoute}>
           <MaterialIcons name="map" size={30} color="white" />
         </TouchableOpacity>
 
         {/* Right Button - Recenter Live Location */}
-        <TouchableOpacity
-          style={styles.recenterLiveButton}
-          onPress={recenterToLiveLocation}
-        >
+        <TouchableOpacity style={styles.recenterLiveButton} onPress={recenterToLiveLocation}>
           <MaterialIcons name="my-location" size={30} color="white" />
         </TouchableOpacity>
 
         {/* Compass Button - Rotate to North */}
-        <TouchableOpacity
-          style={styles.compassButton}
-          onPress={resetMapToNorth}
-        >
+        <TouchableOpacity style={styles.compassButton} onPress={resetMapToNorth}>
           <MaterialIcons name="explore" size={30} color="white" />
         </TouchableOpacity>
 
         {/* Keyboard Handling and Text Inputs */}
-        <KeyboardAvoidingView
-          behavior={Platform.OS === "ios" ? "padding" : "height"}
-          style={styles.avoidingView}
-        >
+        <KeyboardAvoidingView behavior={Platform.OS === "ios" ? "padding" : "height"} style={styles.avoidingView}>
           <View style={styles.textBoxContainer}>
             {/* Dropdown for the first text input */}
             {suggestions1.length > 0 && (
@@ -323,7 +349,6 @@ export default function App() {
               value={text1}
               onChangeText={setText1}
             />
-
             {/* Dropdown for the second text input */}
             {suggestions2.length > 0 && (
               <View style={styles.dropdown}>
